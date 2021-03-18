fd4e5d7851b612b4566988806d4052ccae05e306
Mubaraq Wahab <mub.wahab26@gmail.com>
2021-03-18 17:07:44 +0100

    implement basic parser

--- src/parse.js
+++ src/parse.js
@@ -1,9 +1,55 @@
 const { isOpeningParenthesis, isClosingParenthesis } = require("./identify");
 const { specialForms } = require("./special-forms");
-const { peek, pop } = require("./utilities");
+const { pop } = require("./utilities");
 
-const parenthesize = () => {};
+/**
+ *
+ * @param {array} tokens
+ */
+function parenthesize(tokens) {
+	const token = tokens.shift();
 
-const parse = () => {};
+	if (isOpeningParenthesis(token.value)) {
+		const expression = [];
+		// What happens if there is no closing parenthesis?
+		while (!isClosingParenthesis(tokens[0].value)) {
+			expression.push(parenthesize(tokens));
+		}
+		tokens.shift();
+		return expression;
+	}
+
+	return token;
+}
+
+function parse(tokens) {
+	if (Array.isArray(tokens)) {
+		const [first, ...rest] = tokens;
+		return {
+			type: "CallExpression",
+			name: first.value,
+			arguments: rest.map(parse),
+		};
+	}
+
+	const token = tokens;
+
+	if (token.type === "Number") {
+		return {
+			type: "NumericLiteral",
+			value: token.value,
+		};
+	} else if (token.type === "String") {
+		return {
+			type: "StringLiteral",
+			value: token.value,
+		};
+	} else if (token.type === "Name") {
+		return {
+			type: "Identifier",
+			name: token.value,
+		};
+	}
+}
 
 module.exports = { parse: (tokens) => parse(parenthesize(tokens)) };
