7832dcfc5e38ee580cf43ec7c9bf5d294ce58d71
Mubaraq Wahab <mub.wahab26@gmail.com>
2021-03-18 08:16:25 +0100

    jsdoc tokenize

--- src/tokenize.js
+++ src/tokenize.js
@@ -6,9 +6,16 @@ const {
 	isQuote,
 } = require("./identify");
 
+/**
+ * Creates an array of tokens from a string of Wasp code.
+ * @param {string} input
+ * @returns An array of tokens
+ */
 function tokenize(input) {
 	const tokens = [];
 	return tokens;
 }
+
+// TODO: Check unified ast spec!
 
 module.exports = { tokenize };
