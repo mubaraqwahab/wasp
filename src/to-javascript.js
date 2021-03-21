d268809a21ed1045dc516baf1a0955cc8eda6d06
Mubaraq Wahab <mub.wahab26@gmail.com>
2021-03-21 16:35:58 +0100

    to-js

--- src/to-javascript.js
+++ src/to-javascript.js
@@ -4,12 +4,15 @@ const { traverse } = require("./traverse");
 const babelVisitor = {
 	CallExpression: {
 		enter({ node }) {
-			node.callee = { type: "Identifier" };
+			node.callee = { type: "Identifier", name: node.name, value: node.value };
 		},
 	},
 };
 
-const toJavaScript = (ast) => {};
+const toJavaScript = (ast) => {
+	traverse(ast, babelVisitor);
+	return generate(ast).code;
+};
 
 module.exports = {
 	toJavaScript,
