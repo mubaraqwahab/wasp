6f4d984034797dee6a051a8e0696a8e4d5cac98d
Mubaraq Wahab <mub.wahab26@gmail.com>
2021-04-07 13:52:31 +0100

    review env fns and vars

--- src/environment.ts
+++ src/environment.ts
@@ -1,11 +1,5 @@
 import { Environment, SExpression } from "./types.ts";
 
-function apply<T>(fn: (a: T, b: T) => T) {
-  return function (...list: T[]) {
-    return list.reduce(fn);
-  };
-}
-
 function applyPredicate<T>(pred: (a: T, b: T) => boolean) {
   return (...list: T[]) => {
     for (let i = 0; i < list.length - 1; i++) {
@@ -41,9 +35,7 @@ const defaultEnv: Environment = {
 
     "-": (...numbers: number[]): number => {
       if (!numbers.length) throw new Error(); // expected at least one arg
-
       assertElementsType(numbers, "number");
-
       if (numbers.length === 1) return -numbers[0];
       return numbers.reduce((res, n) => res - n);
     },
@@ -55,7 +47,6 @@ const defaultEnv: Environment = {
 
     "/": (...numbers: number[]): number => {
       if (numbers.length < 2) throw new Error(); // expected at least two args
-
       assertElementsType(numbers, "number");
       return numbers.reduce((res, n) => {
         if (n === 0) throw new Error(); // zero division error
@@ -65,51 +56,88 @@ const defaultEnv: Environment = {
 
     "**": (...numbers: number[]): number => {
       if (numbers.length < 2) throw new Error(); // expected at least two args
-
       assertElementsType(numbers, "number");
       return numbers.reduce((res, n) => res ** n);
     },
 
     "%": (...numbers: number[]): number => {
       if (numbers.length < 2) throw new Error(); // expected at least two args
-
       assertElementsType(numbers, "number");
       return numbers.reduce((res, n) => res % n);
     },
 
     "//": (...numbers: number[]): number => {
       if (numbers.length < 2) throw new Error(); // expected at least two args
-
       assertElementsType(numbers, "number");
       return numbers.reduce(Math.floor);
     },
 
-    "<": applyPredicate<number>((a, b) => a < b),
-    "<=": applyPredicate<number>((a, b) => a <= b),
-    ">": applyPredicate<number>((a, b) => a > b),
-    ">=": applyPredicate<number>((a, b) => a >= b),
+    "<": (...numbers: number[]) => {
+      if (!numbers.length) throw new Error(); // expected at least one arg
+      assertElementsType(numbers, "number");
+      return applyPredicate<number>((a, b) => a < b)(...numbers);
+    },
+
+    "<=": (...numbers: number[]) => {
+      if (!numbers.length) throw new Error(); // expected at least one arg
+      assertElementsType(numbers, "number");
+      return applyPredicate<number>((a, b) => a <= b)(...numbers);
+    },
+
+    ">": (...numbers: number[]) => {
+      if (!numbers.length) throw new Error(); // expected at least one arg
+      assertElementsType(numbers, "number");
+      return applyPredicate<number>((a, b) => a > b)(...numbers);
+    },
+
+    ">=": (...numbers: number[]) => {
+      if (!numbers.length) throw new Error(); // expected at least one arg
+      assertElementsType(numbers, "number");
+      return applyPredicate<number>((a, b) => a >= b)(...numbers);
+    },
+
     "=": applyPredicate((a, b) => a === b),
     "!=": applyPredicate((a, b) => a !== b),
 
     not: (x: SExpression) => !x, // is this good enough?
-    or: apply((a: SExpression, b: SExpression) => a || b),
-    and: apply((a: SExpression, b: SExpression) => a && b),
+    or: (...list: SExpression[]) => list.reduce((res, e) => res || e),
+    and: (...list: SExpression[]) => list.reduce((res, e) => res && e),
 
-    max: Math.max,
-    min: Math.min,
-    print: console.log,
-    type: (val: SExpression) => Array.isArray(val) ? "list" : typeof val,
+    max: (...numbers: number[]) => {
+      assertElementsType(numbers, "number");
+      return Math.max(...numbers);
+    },
 
-    first: (a: SExpression, ..._: SExpression[]) => a,
+    min: (...numbers: number[]) => {
+      assertElementsType(numbers, "number");
+      return Math.min(...numbers);
+    },
+
+    print: (...values: SExpression[]) => {
+      console.log(...values);
+    },
+
+    type: (val: SExpression) => {
+      if (typeof val === "undefined") throw new Error(); // Expected one arg
+      return Array.isArray(val) ? "list" : typeof val;
+    },
+
+    first: (...list: SExpression[]) => list[0],
     last: (...list: SExpression[]) => list[list.length - 1],
     nth: (n: number, ...list: SExpression[]) => list[n],
+    // review what follows
     rest: (_: SExpression, ...others: SExpression[]) => others,
     cons: (first: SExpression, ...rest: SExpression[]) => [first, ...rest],
     list: (...elements: SExpression[]) => elements,
   },
+
   variables: {
+    inf: Infinity,
+    "-inf": -Infinity,
     pi: Math.PI,
   },
 };
+
+const symbols = ["true", "false", "nil"];
 
 export { defaultEnv };
